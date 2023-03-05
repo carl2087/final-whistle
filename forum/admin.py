63126b83db8e3bca1f7f447232a0336d3220d803
Carl <carlelli5@hotmail.co.uk>
2023-03-05 15:59:15 +0000

    added view and templating code to display posts on home page

--- forum/admin.py
+++ forum/admin.py
@@ -1,19 +1,17 @@
 from django.contrib import admin
 from .models import Post, Comment
-from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Post)
-class PostAdmin(SummernoteModelAdmin):
+class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'created_on')
     list_display = ('title', 'slug', 'status', 'team', 'tags', 'created_on')
     search_fields = ('title', 'post_content', 'team', 'tags')
-    summernote_fields = ('post_content', 'excerpt')
 
 
 @admin.register(Comment)
-class CommentAdmin(SummernoteModelAdmin):
+class CommentAdmin(admin.ModelAdmin):
     list_display = ('name', 'tags', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
     search_fields = ('name', 'email', 'comment')
