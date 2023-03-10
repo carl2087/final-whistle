87356c894fb6fa64580cf0b89cc0ae4b718074de
Carl <carlelli5@hotmail.co.uk>
2023-03-10 12:16:14 +0000

    updated post detail html and css

--- static/js/script.js
+++ static/js/script.js
@@ -2,6 +2,10 @@ document.addEventListener('DOMContentLoaded', function () {
     // sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // material boxed initialization
+    let materialboxed = document.querySelectorAll('.materialboxed');
+    M.Materialbox.init(materialboxed);
 });
 
 // adds correct year to copyright in footer
