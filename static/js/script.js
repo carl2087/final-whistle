718d456b8685fba067cbca8326e0d94330981b9e
Carl <carlelli5@hotmail.co.uk>
2023-03-19 14:41:10 +0000

    added a back to top button that appears for mobile devices

--- static/js/script.js
+++ static/js/script.js
@@ -16,8 +16,35 @@ document.addEventListener('DOMContentLoaded', function () {
     setTimeout(function () {
         $('#message-alert').fadeOut('fast');
     },2000)
+
+
+
 });
 
 // adds correct year to copyright in footer
 
 $("#copyright").text(new Date().getFullYear());
+
+// scroll to top button code was sourced from W3schools
+
+let myButton = document.getElementById("top-button");
+
+// When the user scrolls down 600px from the top of the document, show the button
+// set at 600px as only needed on mobile devices
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+        myButton.style.display = "block";
+    } else {
+        myButton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
